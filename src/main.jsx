3b4f0ecaf8e7c576d2573eadf5e8dab8f9c17bf7
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-01 01:42:29 +0500

    header component added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Header from './components/header.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Header />
 )
