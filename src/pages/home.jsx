f1986e6afd15f9769de2e4ff55840641a52d9747
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-03 03:43:34 +0500

    hero section completed fully responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,24 @@
+import { Button } from "antd";
 
-export default function Home(){
+export default function Home() {
 
-    return(
+    return (
         <>
-        <div>
-<div className="hero "></div>
+            <div>
+                <div className="hero "></div>
 
-<div className="flex justify-center flex-col text-center my-6">
-<h1 className="text-5xl font-roboto">DYNAMIC. DOMINANT</h1>
-<h5>AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
-<button className="bg-black text-white "> Shop</button>
-</div>
+                <div className="flex justify-center flex-col text-center my-6 mx-auto w-1/2  font-bold">
+                    <h1 className="text-8xl font-mono">DYNAMIC. DOMINANT</h1>
+                    <h5 className="my-4 text-2xl font-bold font-sans">AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
+                    <div className=" my-2 ">
+                        <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">Shop</Button>
+                    </div>
+                </div>
 
 
 
-        </div>
+
+            </div>
 
 
 
