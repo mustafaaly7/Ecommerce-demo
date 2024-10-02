2956b858ca745b1c5e3191c875b7d075ec83542c
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-02 20:14:36 +0500

    hero section created

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,21 @@ export default function Home(){
 
     return(
         <>
-        <h1 className="my-6 text-6xl"> HOME</h1>
+        <div>
+<div className="hero "></div>
+
+<div className="flex justify-center flex-col text-center my-6">
+<h1 className="text-5xl font-roboto">DYNAMIC. DOMINANT</h1>
+<h5>AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
+<button className="bg-black text-white "> Shop</button>
+</div>
+
+
+
+        </div>
+
+
+
         </>
     )
 }
