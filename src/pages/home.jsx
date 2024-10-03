65c9c5ea948c39b4124783ee0400e881e7392ec4
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-04 01:19:10 +0500

    not completed

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,20 +5,37 @@ export default function Home() {
     return (
         <>
             <div>
-                <div className="hero "></div>
+                {/* hero and details */}
+                <div className="hero ">
+
+                </div>
 
                 <div className="flex justify-center flex-col text-center my-6 mx-auto w-1/2  font-bold">
-                    <h1 className="text-8xl font-mono">DYNAMIC. DOMINANT</h1>
+                    <h1 className="text-8xl font-mono text-center">DYNAMIC. DOMINANT</h1>
                     <h5 className="my-4 text-2xl font-bold font-sans">AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
                     <div className=" my-2 ">
                         <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">Shop</Button>
                     </div>
                 </div>
 
-
-
-
             </div>
+
+
+{/* hero layout  */}
+<div className="flex flex-col">
+    <div className="flex flex-col md:flex-row sm:flex-row w-full">
+        <div className="hero1 flex-1 min-h-[800px]"></div>
+        <div className="hero2 flex-1 min-h-[800px]"></div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
 
 
 
