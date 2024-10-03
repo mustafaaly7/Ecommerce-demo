89b128a1923e0035c1789a0000f03d8ef91e5e12
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-04 01:25:42 +0500

    layout 2 images added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,8 +11,8 @@ export default function Home() {
                 </div>
 
                 <div className="flex justify-center flex-col text-center my-6 mx-auto w-1/2  font-bold">
-                    <h1 className="text-8xl font-mono text-center">DYNAMIC. DOMINANT</h1>
-                    <h5 className="my-4 text-2xl font-bold font-sans">AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
+                    <h1 className="text-4xl  md:text-2xl sm:text-xl font-mono text-center">DYNAMIC. DOMINANT</h1>
+                    <h5 className="my-4 text-lg  font-sans">AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
                     <div className=" my-2 ">
                         <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">Shop</Button>
                     </div>
