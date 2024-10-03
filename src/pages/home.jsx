e6b329456206e9d59f3e7e8f0e2c3f21e32545ee
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-04 01:41:39 +0500

    font responsiveness

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,8 +11,8 @@ export default function Home() {
                 </div>
 
                 <div className="flex justify-center flex-col text-center my-6 mx-auto w-1/2  font-bold">
-                    <h1 className="text-6xl  md:text-4xl sm:text-xl font-mono text-center mx-auto">DYNAMIC. DOMINANT</h1>
-                    <h5 className="my-4 text-lg  font-sans">AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
+                    <h1 className="text-3xl  md:text-3xl sm:text-xl font-mono text-center mx-auto">DYNAMIC. DOMINANT</h1>
+                    <h5 className="my-4   font-sans">AJ Brown and the Air max Dn Are Changing the game in their respective fields</h5>
                     <div className=" my-2 ">
                         <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">Shop</Button>
                     </div>
