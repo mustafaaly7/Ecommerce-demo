309153b37a136b7616155149ad07e7b35eec339d
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-04 21:17:11 +0500

    home page almost done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -41,7 +41,7 @@ export default function Home() {
                             <h1 className="text-white font-sans text-6xl">The Latest Nike Bras</h1>
                             <div className="my-2">
                                 <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">
-                                    Shop 
+                                    Shop
                                 </Button>
                             </div>
                         </div>
@@ -51,23 +51,23 @@ export default function Home() {
 
                 <div className="flex flex-col md:flex-row sm:flex-row w-full">
                     <div className="hero3 flex-1 min-h-[500px] relative">
-                    <div className="absolute bottom-3 left-3 p-2 flex flex-col gap-8">
+                        <div className="absolute bottom-3 left-3 p-2 flex flex-col gap-8">
                             <h1 className="text-white font-mono text-2xl">Distant Gratification</h1>
                             <h1 className="text-white font-sans text-6xl">Nike Pegasus trail 5 Gore-Trex</h1>
                             <div className="my-2">
                                 <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">
-                                    Shop 
+                                    Shop
                                 </Button>
                             </div>
                         </div>
                     </div>
                     <div className="hero4 flex-1 min-h-[500px] relative">
-                    <div className="absolute bottom-3 left-3 p-2 flex flex-col gap-8">
+                        <div className="absolute bottom-3 left-3 p-2 flex flex-col gap-8">
                             <h1 className="text-white font-mono text-2xl">Heat Check</h1>
                             <h1 className="text-white font-sans text-6xl">Air Jordan 4 RM</h1>
                             <div className="my-2">
                                 <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">
-                                    Shop 
+                                    Shop
                                 </Button>
                             </div>
                         </div>
@@ -75,10 +75,18 @@ export default function Home() {
                 </div>
             </div>
 
-            
+            <div className="flex justify-center flex-col text-center my-6 mx-auto w-1/2  font-bold">
+                    <h1 className="text-4xl  md:text-3xl sm:text-xl font-mono text-center mx-auto">NEW THIS WEEK</h1>
+                    <h5 className="my-4   font-sans">Featuring the Nike Tech Woven Suit</h5>
+                    <div className=" my-2 ">
+                        <Button className="heroBtn bg-black text-white hover:bg-white hover:text-black font-bold">Shop New Arrivals</Button>
+                    </div>
+                </div>
 
-            
-            
+<div>
+    <div className="hero5"></div>
+</div>
+
 
 
 
