b49a5f583f32a3b53e5ea80ab54165920da09f85
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-13 23:32:59 +0500

    added product details api'

--- src/components/productDetail.jsx
+++ src/components/productDetail.jsx
@@ -1,7 +1,7 @@
 
 
 
-export default function ProductDetailCard() {
+export default function ProductDetailCard({product}) {
 
     return (
         <>
@@ -11,14 +11,14 @@ export default function ProductDetailCard() {
                         <img
                             alt="ecommerce"
                             className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                            src="https://dummyimage.com/400x400"
+                            src={product?.thumbnail}
                         />
                         <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                             <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                                BRAND NAME
+                                {product?.category}
                             </h2>
                             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                                The Catcher in the Rye
+                                {product?.title}
                             </h1>
                             <div className="flex mb-4">
                                 <span className="flex items-center">
@@ -119,11 +119,7 @@ export default function ProductDetailCard() {
                                 </span>
                             </div>
                             <p className="leading-relaxed">
-                                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps
-                                cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine
-                                tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean
-                                shorts keytar banjo tattooed umami cardigan.
+                                {product?.description}
                             </p>
                             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                                 <div className="flex">
@@ -159,7 +155,7 @@ export default function ProductDetailCard() {
                             </div>
                             <div className="flex">
                                 <span className="title-font font-medium text-2xl text-gray-900">
-                                    $58.00
+                                    ${product?.price}
                                 </span>
                                 <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                                     Button
--- src/pages/productDetail.jsx
+++ src/pages/productDetail.jsx
@@ -1,13 +1,41 @@
 import ProductDetailCard from "@/components/productDetail";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 
 export default function ProductDetail(){
 const {id} = useParams()
+// console.log(id);
+const fetchApi =async()=>{
+let res =await fetch(`https://dummyjson.com/products/${id}`)
+let data = await res.json()
+
+return data
+
+}
+
+
+const{data: product ,isLoading ,isError } = useQuery({
+    queryKey : ["singleProduct"]
+    , queryFn : fetchApi
+})
+
 
     return(
 
         <>
-<ProductDetailCard/>            </>
+
+        {isLoading? (
+                                <h1 className="text-6xl text-center my-6"> Loading..... </h1>
+                                
+        ):(
+<ProductDetailCard product={product}/>           
+
+        )
+        }
+
+
+
+ </>
     )
 
 }
