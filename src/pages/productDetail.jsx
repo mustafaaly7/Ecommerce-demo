f4d691f476af01d58579a4feb4d2a1bf17a45ce1
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-13 23:19:41 +0500

    added product detail page

--- src/pages/productDetail.jsx
+++ src/pages/productDetail.jsx
@@ -1,13 +1,13 @@
+import ProductDetailCard from "@/components/productDetail";
+import { useParams } from "react-router";
 
 export default function ProductDetail(){
+const {id} = useParams()
 
     return(
 
         <>
-        <h1>
-            PRODUCT DETAIL
-            </h1>
-            </>
+<ProductDetailCard/>            </>
     )
 
 }
